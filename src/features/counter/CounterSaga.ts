056e6946be715ef5fa45640b1b26c2c8dde4296d
Quan Nguyen <duyquan238@gmail.com>
2021-09-07 22:32:13 +0700

    config api and model for student and city

--- src/features/counter/CounterSaga.ts
+++ src/features/counter/CounterSaga.ts
@@ -1,12 +1,21 @@
-import { takeEvery } from '@redux-saga/core/effects';
+import { delay, put, takeEvery } from '@redux-saga/core/effects';
 import { PayloadAction } from '@reduxjs/toolkit';
-import { increment } from './counterSlice';
+import { increment, incrementSaga, incrementSuccess } from './counterSlice';
 
-export function* log(action: PayloadAction) {
-  console.log('Log', action);
+// export function* log(action: PayloadAction) {
+//   console.log('Log', action);
+// }
+
+function* handleIncrementSaga(action: PayloadAction<number>) {
+  console.log('Waiting 2s');
+  //wait 2s
+  yield delay(2000);
+  console.log('Waiting done and dispatch action');
+  //dispatch action
+  yield put(incrementSuccess(action.payload));
 }
 
 export default function* counterSaga() {
   console.log('Counter Saga');
-  yield takeEvery(increment().type, log);
+  yield takeEvery(incrementSaga.toString(), handleIncrementSaga);
 }
